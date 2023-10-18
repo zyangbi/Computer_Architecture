54d874c4accd662fb51ee9b409fa9f79ce356f73
zyangbi <62045232+zyangbi@users.noreply.github.com>
2023-10-18 12:53:00 -0700

    assignment2 created

---
+++ assignment2/ChampSim-Intel/create_submitarchive.sh
@@ -0,0 +1,1 @@
+tar -czvf submission.tar.gz *.pdf bin	build_bip.sh build_gimmick.sh build_ptreelru.sh inc Makefile prefetcher replacement branch build_baselines.sh build_dip.sh build_lip.sh compile_champsim.sh LICENSE obj results src
