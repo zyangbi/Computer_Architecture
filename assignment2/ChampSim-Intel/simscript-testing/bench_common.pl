307594ddf029ec741fa106bcec0ec7f89dc5367a
zyangbi <62045232+zyangbi@users.noreply.github.com>
2023-10-19 23:19:25 -0700

    LIP

--- assignment2/ChampSim-Intel/simscript-testing/bench_common.pl
+++ assignment2/ChampSim-Intel/simscript-testing/bench_common.pl
@@ -1,91 +1,91 @@
 %SUITES = ();
 
 $SUITES{'test'}      =
-'hashed_perceptron-no-no-no-no-lru-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz'; 
+'hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz'; 
 
 $SUITES{'test_name'}      =
 '401.bzip2'; 
 
 #***************************** SPEC 2006 **********************************
 $SUITES{'lru'}      =
-'hashed_perceptron-no-no-no-no-lru-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-lru-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-lru-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-lru-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-lru-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-lru-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'srrip'}      =
-'hashed_perceptron-no-no-no-no-srrip-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-srrip-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-srrip-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-srrip-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-srrip-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-srrip-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-srrip-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-srrip-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-srrip-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-srrip-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-srrip-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-srrip-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'drrip'}      =
-'hashed_perceptron-no-no-no-no-drrip-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-drrip-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-drrip-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-drrip-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-drrip-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-drrip-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-drrip-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-drrip-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-drrip-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-drrip-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-drrip-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-drrip-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'ship'}      =
-'hashed_perceptron-no-no-no-no-ship-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-ship-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-ship-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-ship-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-ship-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-ship-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-ship-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-ship-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-ship-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-ship-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-ship-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-ship-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'hawkeye'}      =
-'hashed_perceptron-no-no-no-no-hawkeye-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-hawkeye-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-hawkeye-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-hawkeye-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-hawkeye-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-hawkeye-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-hawkeye-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-hawkeye-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-hawkeye-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-hawkeye-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-hawkeye-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-hawkeye-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'ptreelru'}      =
-'hashed_perceptron-no-no-no-no-ptreelru-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-ptreelru-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-ptreelru-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-ptreelru-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-ptreelru-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-ptreelru-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-ptreelru-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-ptreelru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-ptreelru-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-ptreelru-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-ptreelru-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-ptreelru-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'lip'}      =
-'hashed_perceptron-no-no-no-no-lip-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-lip-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-lip-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-lip-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-lip-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-lip-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-lip-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-lip-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-lip-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-lip-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-lip-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-lip-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'bip'}      =
-'hashed_perceptron-no-no-no-no-bip-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-bip-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-bip-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-bip-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-bip-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-bip-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-bip-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-bip-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-bip-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-bip-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-bip-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-bip-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'dip'}      =
-'hashed_perceptron-no-no-no-no-dip-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-dip-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-dip-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-dip-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-dip-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-dip-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-dip-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-dip-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-dip-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-dip-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-dip-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-dip-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 $SUITES{'gimmick'}      =
-'hashed_perceptron-no-no-no-no-gimmick-4core 10 40 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
-hashed_perceptron-no-no-no-no-gimmick-4core 10 40 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
-hashed_perceptron-no-no-no-no-gimmick-4core 10 40 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
-hashed_perceptron-no-no-no-no-gimmick-4core 10 40 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
-hashed_perceptron-no-no-no-no-gimmick-4core 10 40 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
-hashed_perceptron-no-no-no-no-gimmick-4core 10 40 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+'hashed_perceptron-no-no-no-no-gimmick-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-gimmick-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-gimmick-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-gimmick-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-gimmick-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-gimmick-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
 
 #*****************************SPEC NAMES **********************************
 $SUITES{'spec_name'}      =
