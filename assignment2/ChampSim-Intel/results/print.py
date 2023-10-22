75dbcf901bc8067447c4b78571ff19fbcf1f2d5b
zyangbi <62045232+zyangbi@users.noreply.github.com>
2023-10-22 01:44:47 -0700

    DIP

---
+++ assignment2/ChampSim-Intel/results/print.py
@@ -0,0 +1,19 @@
+import os
+
+directories = ['lru', 'lip', 'bip', 'dip']
+files = ['400.perlbench', '401.bzip2', '403.gcc', '462.libquantum', '464.h264ref', '471.omnetpp']
+
+for file in files:
+    print(file)
+    for directory in directories:
+        print(directory)
+        file_path = os.path.join(directory, file)
+
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as f:
+                for line in f:
+                    # if "ROI_LLCTOTAL_HITRATE" in line:
+                    if "ROI_Cumulative_IPC" in line:
+                        print(line)
+    print()
+    print()
