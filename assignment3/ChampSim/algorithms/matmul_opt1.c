be119f7a4250d3434818620ea7b8f15422781be7
zyangbi <62045232+zyangbi@users.noreply.github.com>
2023-11-21 02:26:52 -0800

    matrix multiply

--- assignment3/ChampSim/algorithms/matmul_opt1.c
+++ assignment3/ChampSim/algorithms/matmul_opt1.c
@@ -2,4 +2,11 @@
 
 void matmul_opt1(unsigned long long int* mat_a, unsigned long long int* mat_b, unsigned long long int* mat_c, unsigned long long int matrix_size){
     //**********************************Add your optimized code below**********************************//
+    for (unsigned long long int j = 0; j < matrix_size; j += 1) {
+        for (unsigned long long int k = 0; k < matrix_size; k += 1) {
+            for (unsigned long long int i = 0; i < matrix_size; i += 1) {
+                mat_c[j*matrix_size + i] += mat_a[k*matrix_size + i] * mat_b[j*matrix_size + k];
+            }
+        }
+    }
 }
--- assignment3/ChampSim/algorithms/matmul_opt2.c
+++ assignment3/ChampSim/algorithms/matmul_opt2.c
@@ -2,4 +2,17 @@
 
 void matmul_opt2(unsigned long long int* mat_a, unsigned long long int* mat_b, unsigned long long int* mat_c, unsigned long long int matrix_size){
     //**********************************Add your optimized code below**********************************//
+    unsigned long long int B = 64 / sizeof(unsigned long long int);  // block size = 64 bytes
+
+    for (unsigned long long int kk = 0; kk < matrix_size; kk += B) {
+        for (unsigned long long int ii = 0; ii < matrix_size; ii += B) {
+            for (unsigned long long int j = 0; j < matrix_size; j++) {
+                for (unsigned long long int k = kk; k < kk + B && k < matrix_size; k++) {
+                    for (unsigned long long int i = ii; i < ii + B && i < matrix_size; i++) {
+                        mat_c[j*matrix_size + i] += mat_a[k*matrix_size + i] * mat_b[j*matrix_size + k];
+                    }
+                }
+            }
+        }
+    }
 }
