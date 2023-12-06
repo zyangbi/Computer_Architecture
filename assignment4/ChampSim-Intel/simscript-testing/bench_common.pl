afc51cc724ba1099611337510d5b6d9eea793242
zyangbi <62045232+zyangbi@users.noreply.github.com>
2023-12-06 14:07:56 -0800

    initialization

---
+++ assignment4/ChampSim-Intel/simscript-testing/bench_common.pl
@@ -0,0 +1,49 @@
+%SUITES = ();
+
+$SUITES{'test'}      =
+'hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz'; 
+
+$SUITES{'test_name'}      =
+'401.bzip2'; 
+
+#*****************************SPEC 406 **********************************
+$SUITES{'hashed_perceptron'}      =
+'hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_perceptron-no-no-no-no-lru-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+
+$SUITES{'perceptron'}      =
+'perceptron-no-no-no-no-lru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+perceptron-no-no-no-no-lru-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+perceptron-no-no-no-no-lru-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+perceptron-no-no-no-no-lru-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+perceptron-no-no-no-no-lru-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+perceptron-no-no-no-no-lru-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+
+$SUITES{'2bitcorr'}      =
+'2bitcorr-no-no-no-no-lru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+2bitcorr-no-no-no-no-lru-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+2bitcorr-no-no-no-no-lru-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+2bitcorr-no-no-no-no-lru-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+2bitcorr-no-no-no-no-lru-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+2bitcorr-no-no-no-no-lru-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+
+$SUITES{'hashed_gselect'}      =
+'hashed_gselect-no-no-no-no-lru-4core 1 4 0 0 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz 401.bzip2.gz
+hashed_gselect-no-no-no-no-lru-4core 1 4 0 0 403.gcc.gz 403.gcc.gz 403.gcc.gz 403.gcc.gz
+hashed_gselect-no-no-no-no-lru-4core 1 4 0 0 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz 462.libquantum.gz
+hashed_gselect-no-no-no-no-lru-4core 1 4 0 0 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz 464.h264ref.gz
+hashed_gselect-no-no-no-no-lru-4core 1 4 0 0 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz 400.perlbench.gz
+hashed_gselect-no-no-no-no-lru-4core 1 4 0 0 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz 471.omnetpp.gz';
+
+#*****************************SPEC NAMES **********************************
+$SUITES{'spec_name'}      =
+'401.bzip2
+403.gcc
+462.libquantum
+464.h264ref.gz
+400.perlbench
+471.omnetpp.gz';
